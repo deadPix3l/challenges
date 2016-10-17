521fa8d45a336fd8ca15e1ad11b24c4538f7477a
deadpix3l <skylerr.curtis@gmail.com>
2016-10-17 14:51:42 +0000

    fixing random git junk from merge

--- Adventofcode/day2.py
+++ Adventofcode/day2.py
@@ -7,14 +7,9 @@ def prod(x):
     y = 1
     for i in x: y*=i
     return y
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 02f7717a8e75915717f4575d35cfc5c982ee5566
 def sides(x): return [prod(x)/i for i in x]
 
-    
 # the solve!
 print "part1:", sum(min(sides(i))+2*sum(sides(i)) for i in x)
 print "part2:", sum(2*(sum(i)-max(i))+prod(i) for i in x)
