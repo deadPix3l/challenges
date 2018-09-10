2d8dbb6c250212c5068483087fc5eb0d0a6ce53c
deadpix3l <skylerr.curtis@gmail.com>
2018-09-10 22:46:54 +0000

    bonus points: 364

--- Dailyprogrammer/easy/364.rb
+++ Dailyprogrammer/easy/364.rb
@@ -3,5 +3,6 @@
 prng = Random.new()
 ARGF.readlines.each do |i|
    cnt,die = i.split('d').map { |x| x.to_i }
-   puts (1..cnt).map { prng.rand(die)+1 }.inject(:+)
+   y = (1..cnt).map { prng.rand(die)+1 }
+   puts "#{cnt}d#{die} - #{y.inject(:+)}: #{y.join(' ')}"
 end
